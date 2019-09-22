9fb65d45a49dba4993ec9317ff8f74ab556e1f14
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-22 19:37:30 -0400

    Changed backend to port:2181.  Allowed for cross-origin.  Added helmet.

--- back-end/app.js
+++ back-end/app.js
@@ -2,11 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet());
+
+// Allow cross-origin.....
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
