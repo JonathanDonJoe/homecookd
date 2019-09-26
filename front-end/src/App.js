7d9841e884d27ced44b5e1e0b2eac8c74463ba9c
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-26 00:19:22 -0400

    Connected app to the Firebase database.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sanity Check</h1>
-    </div>
-  );
+import * as firebase from 'firebase';
+
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            text: '10'
+        }
+    }
+
+    componentDidMount() {
+
+        const rootRef = firebase.database().ref().child('react').child('data').child('user').child('user1');
+        rootRef.on('value', snap => {
+            console.log(snap)
+            this.setState({
+                text: snap.val()
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>{this.state.text}</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import * as firebase from 'firebase';
+import firebaseConfig from './components/Firebase/firebase';
+
+firebase.initializeApp(firebaseConfig);
+
+
+
 ReactDOM.render( 
     <App />
     , 
