5a7321523ca21cbb077d5705b7dc3bd3f6dfe9ad
Anthony Pellingra <anthonypellingra@gmail.com>
2019-10-01 14:32:29 -0400

    made hero image that rotates image and text based on current time

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Sanity Check</h1>
+      <Home />
     </div>
   );
 }
