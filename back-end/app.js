31b719aa959eb1c81f8a183e5d6a19f3875d822d
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-10-02 10:46:03 -0400

    Linked events route

--- back-end/app.js
+++ back-end/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 app.use(helmet());
@@ -26,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
 
 module.exports = app;
