e7db3e7c2ac172081630e0cdc86c008c32d3b4b2
Nathan Evans <nevans.dev@gmail.com>
2019-10-03 13:30:58 -0400

    here is a commit to clear my git tree.

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,7 +2,28 @@ var express = require('express');
 var router = express.Router();
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const request = require('request');
 
+var getOptions = { method: 'GET',
+  url: 'https://dev-ag0cp9dk.auth0.com/api/v2/',
+  headers: { authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5qSTBSREU1UVRjME1rTkZRalZHUVVKQ01VTTFRMFZETUVGRU1EVkNNVVU1TVRVeVFUYzJNZyJ9.eyJpc3MiOiJodHRwczovL2Rldi1hZzBjcDlkay5hdXRoMC5jb20vIiwic3ViIjoicXlKcTFNaHRYYlFORHhlTFh1MXd0Tks0aFdES0l1VmVAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LWFnMGNwOWRrLmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNTcwMTE0MjY2LCJleHAiOjE1NzAyMDA2NjYsImF6cCI6InF5SnExTWh0WGJRTkR4ZUxYdTF3dE5LNGhXREtJdVZlIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.C-TzawdVIg9BLBAP2LeeWSr-_05o2Mqhamy9XJ7jmvogOfKxHBJNdfhaG4lLB13cXlra6aouWMBOcg-I7iiMbIC133s0U5iRpo_txmQH3zjJozcJf_oWX0Ehk_dr64a6qQtaTicBcdn_edBlDoRkzs6GbdLKcayvkpigNCPcE7BCc3V3SnnOce_okQAbiuY0Ba6fnr0jaVn41MYCpTp69gdMSvWUb1o1dfDrAGUiHecNlTQBNiENd3SeP2-R869F6RHTXB4oYhmCD-S4AUj42EVZbFi4YF9z5iZ4IRfTtWLGaNpBfNEu4OMKgtqIlYGpulrkfaxJlN2Hiy3Zoux-Ew' } };
+
+request(getOptions, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+var postOptions = { method: 'POST',
+  url: 'https://dev-ag0cp9dk.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: '{"client_id":"qyJq1MhtXbQNDxeLXu1wtNK4hWDKIuVe","client_secret":"IfDdIqcjgqjq6dnEZbS9esIq857Vh5BvUq3Uy8KM_XFP3tpN6HE3DvrBZf3hEwcN","audience":"https://dev-ag0cp9dk.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+
+request(postOptions, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
 
 const db = require('../db');
 
--- front-end/src/components/Auth/Auth.js
+++ front-end/src/components/Auth/Auth.js
@@ -6,7 +6,7 @@ class Auth {
         clientID: `1GhYuE5mUY005Y6imP9Auc2R7smNW848`,
         domain: `dev-ag0cp9dk.auth0.com`,        
         redirectUri: `http://localhost:3000/callback`,
-        audience: `https://dev-ag0cp9dk.auth0.com/userinfo`,
+        audience: `https://dev-ag0cp9dk.auth0.com/api/v2/`,
         responseType: `id_token`,
         scope: `openid profile`
     });
