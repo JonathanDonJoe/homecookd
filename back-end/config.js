2917404f4ed856870dee757302f1c6c8836e00ea
Nathan Evans <nevans.dev@gmail.com>
2019-10-03 09:28:44 -0400

    Here is the code for the Auth0. It works when you click on login and on register.

---
+++ back-end/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        host: 'localhost',
+        database: 'capstone',
+        user: 'capstone_user',
+        password: 'x'
+    }
+}
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 
 const db = require('../db');
 
-router.post('*', (req, res, next) => {
-    const token = req.body.token
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-ag0cp9dk.auth0.com/.well-known/jwks.json`
+    }),
+    audience: `1GhYuE5mUY005Y6imP9Auc2R7smNW848`,
+    issuer: `dev-ag0cp9dk.auth0.com`,
+    algorithms: [`RS256`]
+})
+
+router.post('*', checkJwt, (req, res, next) => {
+    const { token, first_name, last_name, email, picture } = req.body;
+    console.log(req.body);
+    // const token = req.body.token
     console.log('index token: ')
     console.log(token)
     const getUserIdQuery = `SELECT id FROM users WHERE token = ?`;
