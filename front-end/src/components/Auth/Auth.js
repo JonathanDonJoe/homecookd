0fa0644dbb1b3b03bfd9273cdc6ce5bb518f8e71
Jonathan Joe <52137110+JonathanDonJoe@users.noreply.github.com>
2019-10-04 10:29:59 -0400

    Update Auth.js

--- front-end/src/components/Auth/Auth.js
+++ front-end/src/components/Auth/Auth.js
@@ -3,55 +3,6 @@ import auth_config from '../../config';
 import axios from 'axios';
 
 class Auth {
-  constructor() {
-    this.auth0 = new auth0.WebAuth(auth_config.auth);
-    this.getProfile = this.getProfile.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-    this.signIn = this.signIn.bind(this);
-    this.signOut = this.signOut.bind(this);
-  }
-
-  getProfile() {
-    return this.profile;
-  }
-
-  getIdToken() {
-    return this.idToken;
-  }
-
-  isAuthenticated() {
-    return new Date().getTime() < this.expiresAt;
-  }
-
-  signIn() {
-    this.auth0.authorize();
-  }
-
-  handleAuthentication() {
-    return new Promise((resolve, reject) => {
-      this.auth0.parseHash((err, authResult) => {
-        if (err) return reject(err);
-        if (!authResult || !authResult.idToken) {
-          return reject(err);
-        }
-        this.idToken = authResult.idToken;
-        this.profile = authResult.idTokenPayload;
-
-        console.log('Signed in. POST to express here')
-
-        // set the time that the id token will expire at
-        this.expiresAt = authResult.idTokenPayload.exp * 1000;
-        resolve();
-      });
-    })
-  }
-
-  signOut() {
-    // clear id token, profile, and expiration
-    this.idToken = null;
-    this.profile = null;
-    this.expiresAt = null;
   }
     constructor() {
         this.auth0 = new auth0.WebAuth(auth_config.auth);
