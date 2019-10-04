e16c64c3beee2fe2284e1c438fbf673afe2e8693
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-10-04 10:57:09 -0400

    laid foundation for chatbar, event cards, and fixed nav positioning

--- front-end/src/components/SearchBar/SearchBar.js
+++ front-end/src/components/SearchBar/SearchBar.js
@@ -7,15 +7,15 @@ export class SearchBar extends Component {
                 <div className="search-row">
                     <form>
                         <h3>Find some Fresh Grub</h3>
-                        <div class="inner-form">
-                            <div class="input-field first-wrap">
+                        <div className="inner-form">
+                            <div className="input-field first-wrap">
                                 <input id="search" type="text" placeholder="What are you looking for?" />
                             </div>
-                            <div class="input-field second-wrap">
+                            <div className="input-field second-wrap">
                                 <input id="location" type="text" placeholder="location" />
                             </div>
-                            <div class="input-field third-wrap">
-                                <button class="btn-search" type="button">Search</button>
+                            <div className="input-field third-wrap">
+                                <button className="btn-search" type="button">Search</button>
                             </div>
                         </div>
                     </form>
