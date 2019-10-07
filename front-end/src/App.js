f42da30c6640c61aabcdb5a815a2ae3b284caa3e
Nathan Evans <nevans.dev@gmail.com>
2019-10-07 09:28:04 -0400

    Here is the form for creating events.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Callback from './components/Auth/Callback';
 import ChatBar from './components/ChatBar/ChatBar';
+import EventCreate from './components/EventCreate/EventCreate';
 
 function App() {
     return (
@@ -14,9 +15,9 @@ function App() {
 				<Route path='/' component={NavBar} />
 				<Route path='/users' component={ChatBar} />
 				<Route exact path='/' component={Home} />
-				<Route exact path='/callback' component={Callback} />
+				<Route exact path='/callback' component={Callback} />				
+				<Route exact path='/host' component={EventCreate} />
 				{/* <Route exact path='/events' component={Events} /> */}
-				{/* <Route exact path='/login' component={LoginPage} /> */}
 				{/* <Route exact path='/register' component={RegisterPage} /> */}
       		</div>
         </Router>
