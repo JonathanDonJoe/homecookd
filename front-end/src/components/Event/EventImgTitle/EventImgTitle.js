3b921f589b425da2dd8367ea42e9d2020668e1ff
Nathan Evans <nevans.dev@gmail.com>
2019-10-08 09:47:50 -0400

    Didn't work on much over the night, but here is what I have.

--- front-end/src/components/Event/EventImgTitle/EventImgTitle.js
+++ front-end/src/components/Event/EventImgTitle/EventImgTitle.js
@@ -5,9 +5,9 @@ export class EventImgTitle extends Component {
     render() {
         return (
             <div className='row'>
-                <div className='event-hero col s12 m4' style={{backgroundImage: `url(../back-end${this.props.event.event_picture})`}}>
+                {/* <div className='event-hero col s12 m4' style={{backgroundImage: `url(../back-end${this.props.event.event_picture})`}}>
 
-                </div>
+                </div> */}
                 <div className='event-text col s12 m8'>
                     <h3>{this.props.event.event_title}</h3>
                     <p> 
