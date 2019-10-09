763f59f9b6f7b96ce0e7b9bdf35f17177d27b04d
Anthony Pellingra <anthonypellingra@gmail.com>
2019-10-09 14:27:46 -0400

    google maps

---
+++ front-end/src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,125 @@
+
+import React, { Component } from 'react';
+import { GoogleApiWrapper, Map, InfoWindow, Marker, Circle } from 'google-maps-react';
+
+export class MapContainer extends Component {
+  state = {
+   showingInfoWindow: false,  //Hides or the shows the infoWindow
+   activeMarker: {},          //Shows the active marker upon click
+   selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+ };
+
+ onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ }
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+render() {
+      const style = {
+      width: '100vw',
+      height: '95vh',
+      marginTop: '5vh'
+    }
+  return (
+    <Map
+      google={this.props.google}
+      zoom={14}
+      style={style}
+      initialCenter={{ lat: 33.848996, lng: -84.373326 }}
+    >
+      <Circle
+      radius={1}
+      />
+      <Marker
+        onClick={this.onMarkerClick}
+        name={'Atlanta Tech Village'}
+       />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <a href='https://www.google.com/maps/dir//3150+Roswell+Rd+NW,+Atlanta,+GA+30305/'><h4>Big Summer Blowout</h4></a>
+        </div>
+      </InfoWindow>
+    </Map>
+  );
+}
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDkza9FbkxpbYN5u7ZmXpkQJx5NKtm1u9s'
+})(MapContainer);
+
+// import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+// import React, { Component } from 'react'
+// import './GoogleMap.css'
+// import InfoWindowEx from './InfoWindowEx';
+
+
+// export class MapContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showingInfoWindow: false,
+//       activeMarker: {},
+//     }
+//   }
+//   onMarkerClick = (props, marker, e) =>{
+
+//     this.setState({
+//       showingInfoWindow: true,
+//       activeMarker: marker
+//     });
+//     console.log('Show me the info');
+//   }
+
+//   render() {
+//     const style = {
+//       width: '100vw',
+//       height: '100vh'
+//     }
+//     return (
+//       <div className='map-container'>
+//         <Map
+//           onClick={this.onMapClicked}
+//           google={this.props.google}
+//           style={style}
+//           zoom={13}
+//           initialCenter={{ lat: 33.848996, lng: -84.373326}}
+//         >
+//           <Marker
+//             onClick={this.onMarkerClick}
+//             name={'Atlanta Tech Village'}
+//             position={{lat: 33.848996, lng: -84.373326}}>
+//             <InfoWindowEx
+//               marker={this.state.activeMarker}
+//               visible={this.state.showingInfoWindow}
+//               >
+//               <div>
+//               <h3>Info Test</h3>
+//               </div>
+//             </InfoWindowEx>
+//           </Marker>
+//         </Map>
+//       </div>
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: 'AIzaSyDkza9FbkxpbYN5u7ZmXpkQJx5NKtm1u9s'
+// })(MapContainer);
