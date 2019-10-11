562850e9afc432eeebfe3c96c1f3caaacdc3d983
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-10-11 12:31:33 -0400

    Changed seed for lat/long.  Styled address in EventCreate

--- back-end/seed.js
+++ back-end/seed.js
@@ -6,7 +6,8 @@ let seedData = [
         time: 8,
 
         address: `3150 Roswell Rd NW`, 
-        zipcode: '00000',
+        lat: 0, 
+        lng: 0,
         title: `Pizza Party in Buckhead`,
         description: `I'm making some pizzas in my oven and would love to share them and enjoy some company. Topping bar will be available for dine-in or pick-up`,
         host_id: 1,
@@ -21,7 +22,8 @@ let seedData = [
     {
         time: 16,
         address: `1280 Habersham Rd`,
-        zipcode: '11111',
+        lat: 0, 
+        lng: 0,
         title: `Grill Out`,
         description: `Looking for some people to eat some burgers, dogs, and have a good time on my deck. BYOB`,
         host_id: 1,
@@ -35,7 +37,8 @@ let seedData = [
     {
         time: 24,
         address: `4239 Piedmont Rd`,
-        zipcode: '22222',
+        lat: 0, 
+        lng: 0,
         title: `Pasta Pickup`,
         description: `Made too much pasta, looking to get some off my hands. Will have different sauces and stuff available`,
         host_id: 1,
@@ -45,18 +48,18 @@ let seedData = [
         picture: ``,
         dine_in: 0,
         pick_up: 1,
-    },
+    }
 ]
 
 const insertEventQuery = `
 INSERT INTO events 
-    (time, address, zipcode, title, description, host_id, portions, price, tags, dine_in, pick_up)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    (time, address, lat, lng, title, description, host_id, portions, price, tags, dine_in, pick_up)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 `
 let date = new Date();
 date.setHours(date.getHours() + 1000)
 seedData.forEach((arr,i)=>{
-    let dbValues = [date, arr.address, arr.zipcode, arr.title, arr.description, arr.host_id, arr.portions,
+    let dbValues = [date, arr.address, arr.lat, arr.lng, arr.title, arr.description, arr.host_id, arr.portions,
          arr.price, arr.tags, arr.dine_in, arr.pick_up]
     db.query(insertEventQuery,dbValues)
 })
--- front-end/src/components/EventCreate/EventCreate.js
+++ front-end/src/components/EventCreate/EventCreate.js
@@ -11,7 +11,7 @@ import hostMealAction from '../../actions/hostMealAction';
 
 
 export class EventCreate extends Component {
-    state={
+    state = {
         time: "",
         date: null,
         address: "",
@@ -22,13 +22,13 @@ export class EventCreate extends Component {
         picture: "",
         dineIn: false,
         pickUp: false
-    }  
+    }
 
     changeDate = (date) => {
         this.setState({
             date
         })
-    } 
+    }
 
     // changeTime = (number1, number2) => {
     //     // console.log(number1);
@@ -108,27 +108,27 @@ export class EventCreate extends Component {
         const geocoder = new window.google.maps.Geocoder();
         let address = document.getElementById('autocomplete').value
         return new Promise((resolve, reject) => {
-          geocoder.geocode(
-            {
-              address: address
-            },
-            (results, status) => {
-              if (status === window.google.maps.GeocoderStatus.OK) {
-                let response = results
-                resolve(response);
-              } else {
-                reject(new Error(status));
-              }
-            }
-          );
+            geocoder.geocode(
+                {
+                    address: address
+                },
+                (results, status) => {
+                    if (status === window.google.maps.GeocoderStatus.OK) {
+                        let response = results
+                        resolve(response);
+                    } else {
+                        reject(new Error(status));
+                    }
+                }
+            );
         });
-      }
+    }
 
     onSubmit = async (e) => {
         e.preventDefault();
         const file = document.getElementById('picture-location').files[0];
         let geocodeData = await this.getGeocode()
-        console.log(geocodeData[0]); 
+        console.log(geocodeData[0]);
         let address = geocodeData[0].formatted_address
         let lat = geocodeData[0].geometry.location.lat()
         let lng = geocodeData[0].geometry.location.lng()
@@ -159,17 +159,17 @@ export class EventCreate extends Component {
 
         })
 
-                // if (status == google.maps.GeocoderStatus.OK) {
-                //   loc[0]=results[0].geometry.location.lat();
-                //   loc[1]=results[0].geometry.location.lng();
-          
-                //   alert( loc ); // the place where loc contains geocoded coordinates
-          
-                // } else {
-                //   alert("Geocode was not successful for the following reason: " + status);
-                // }
-      
-      
+        // if (status == google.maps.GeocoderStatus.OK) {
+        //   loc[0]=results[0].geometry.location.lat();
+        //   loc[1]=results[0].geometry.location.lng();
+
+        //   alert( loc ); // the place where loc contains geocoded coordinates
+
+        // } else {
+        //   alert("Geocode was not successful for the following reason: " + status);
+        // }
+
+
 
         // let urlAddress = address.replace(/\s/g, '+');
         // console.log(urlAddress);
@@ -179,7 +179,7 @@ export class EventCreate extends Component {
 
     }
 
-    
+
 
     render() {
         // console.log(this.state);
@@ -191,116 +191,116 @@ export class EventCreate extends Component {
         return (
             <div id="host-form" className="row">
                 <form onSubmit={this.onSubmit} className="col s12">
-                <div className="row">
-                    <div className="input-field col s6 offset-s3">
-                    <input value={this.state.title} onChange={this.changeTitle} maxLength={100} id="input_text" type="text" className="validate" />
-                    <label htmlFor="input_text">Give your dish or party a name!</label>
-                    </div>
+                    <div className="row">
+                        <div className="input-field col s6 offset-s3">
+                            <input value={this.state.title} onChange={this.changeTitle} maxLength={100} id="input_text" type="text" className="validate" />
+                            <label htmlFor="input_text">Give your dish or party a name!</label>
+                        </div>
 
-                    <div className="input-field col s6 offset-s3">
-                    <textarea id="textarea1" value={this.state.description} onChange={this.changeDescription} maxLength={300} className="materialize-textarea"></textarea>
-                    <label htmlFor="textarea1">Describe your party, food, or atmosphere.</label>
+                        <div className="input-field col s6 offset-s3">
+                            <textarea id="textarea1" value={this.state.description} onChange={this.changeDescription} maxLength={300} className="materialize-textarea"></textarea>
+                            <label htmlFor="textarea1">Describe your party, food, or atmosphere.</label>
+                        </div>
+                        <div className="input-field col s6 offset-s3">
+                            <AddressAutocomplete changeAddress={this.changeAddress} />
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <AddressAutocomplete changeAddress={this.changeAddress}/>    
-                </div>
-                <div className="row">        
-                    <div className="input-field col s2 offset-s3">
-                    <select className="browser-default" value={this.state.portions} onChange={this.changePortions}>
-                        <option value="">How many servings?</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
+                    <div className="row">
+                        <div className="input-field col s2 offset-s3">
+                            <select className="browser-default" value={this.state.portions} onChange={this.changePortions}>
+                                <option value="">How many servings?</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
 
-                    </select>
-                    </div>
-                    <div className="input-field col s2">
-                    <select className="browser-default" value={this.state.price} onChange={this.changePrice}>
-                        <option value="">Price per plate?</option>
-                        <option value="1">$1.00</option>
-                        <option value="1.50">$1.50</option>
-                        <option value="2">$2.00</option>
-                        <option value="2.50">$2.50</option>
-                        <option value="3">$3.00</option>
-                        <option value="3.50">$3.50</option>
-                        <option value="4">$4.00</option>
-                        <option value="4.50">$4.50</option>
-                        <option value="5">$5.00</option>
-                        <option value="5.50">$5.50</option>
-                        <option value="6">$6.00</option>
-                        <option value="6.50">$6.50</option>
-                        <option value="7">$7.00</option>
-                        <option value="7.50">$7.50</option>
-                        <option value="8">$8.00</option>
-                        <option value="8.50">$8.50</option>
-                        <option value="9">$9.00</option>
-                        <option value="9.50">$9.50</option>
-                        <option value="10">$10.00</option>
-                        <option value="10.50">$10.50</option>
-                        <option value="11">$11.00</option>
-                        <option value="11.50">$11.50</option>
-                        <option value="12">$12.00</option>
-                        <option value="12.50">$12.50</option>
-                        <option value="13">$13.00</option>
-                        <option value="13.50">$13.50</option>
-                        <option value="14">$14.00</option>
-                        <option value="14.50">$14.50</option>
-                        <option value="15">$15.00</option>
+                            </select>
+                        </div>
+                        <div className="input-field col s2">
+                            <select className="browser-default" value={this.state.price} onChange={this.changePrice}>
+                                <option value="">Price per plate?</option>
+                                <option value="1">$1.00</option>
+                                <option value="1.50">$1.50</option>
+                                <option value="2">$2.00</option>
+                                <option value="2.50">$2.50</option>
+                                <option value="3">$3.00</option>
+                                <option value="3.50">$3.50</option>
+                                <option value="4">$4.00</option>
+                                <option value="4.50">$4.50</option>
+                                <option value="5">$5.00</option>
+                                <option value="5.50">$5.50</option>
+                                <option value="6">$6.00</option>
+                                <option value="6.50">$6.50</option>
+                                <option value="7">$7.00</option>
+                                <option value="7.50">$7.50</option>
+                                <option value="8">$8.00</option>
+                                <option value="8.50">$8.50</option>
+                                <option value="9">$9.00</option>
+                                <option value="9.50">$9.50</option>
+                                <option value="10">$10.00</option>
+                                <option value="10.50">$10.50</option>
+                                <option value="11">$11.00</option>
+                                <option value="11.50">$11.50</option>
+                                <option value="12">$12.00</option>
+                                <option value="12.50">$12.50</option>
+                                <option value="13">$13.00</option>
+                                <option value="13.50">$13.50</option>
+                                <option value="14">$14.00</option>
+                                <option value="14.50">$14.50</option>
+                                <option value="15">$15.00</option>
 
-                    </select>
+                            </select>
+                        </div>
+                        <p className='col s1'>
+                            <label>
+                                <input value={this.state.dineIn} onChange={this.changeDineIn} type="checkbox" />
+                                <span>Dine In</span>
+                            </label>
+                        </p>
+                        <p className='col s1'>
+                            <label>
+                                <input value={this.state.pickUp} onChange={this.changePickUp} type="checkbox" />
+                                <span>Pick-up</span>
+                            </label>
+                        </p>
                     </div>
-                    <p className='col s1'>
-                        <label>
-                            <input value={this.state.dineIn} onChange={this.changeDineIn} type="checkbox" />
-                            <span>Dine In</span>
-                        </label>
-                    </p>
-                    <p className='col s1'>
-                        <label>
-                            <input value={this.state.pickUp} onChange={this.changePickUp} type="checkbox" />
-                            <span>Pick-up</span>
-                        </label>
-                    </p>
-                </div>
                     <div className="row">
                         <div className="file-field input-field col s3 offset-s3">
                             <TimePicker type="text" className="timepicker" placeholder="What time?" />
                         </div>
                         <div className="file-field input-field col s3">
-                        <label htmlFor='date' ></label>
+                            <label htmlFor='date' ></label>
                             <DatePicker type='text' id='date' placeholder='Date' className='datepicker' onChange={this.changeDate} />
                         </div>
                     </div>
-                <div className="row">
-                    <div className="file-field input-field col s6 offset-s3">
-                        <div className="btn">
-                            <span>File</span>
-                            <input id='picture-location' type="file" value={this.state.picture} onChange={this.changePicture} />
-                        </div>
-                        <div className="file-path-wrapper">
-                            <input className="file-path validate" type="text" placeholder="Upload some food pics!!" />
-                        </div>
-                    </div>
-                </div>
                     <div className="row">
-                    <div className="file-field input-field col s6 offset-s5">
-                        <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                            <i className="material-icons right">send</i>
-                        </button>
+                        <div className="file-field input-field col s6 offset-s3">
+                            <div className="btn">
+                                <span>File</span>
+                                <input id='picture-location' type="file" value={this.state.picture} onChange={this.changePicture} />
+                            </div>
+                            <div className="file-path-wrapper">
+                                <input className="file-path validate" type="text" placeholder="Upload some food pics!!" />
+                            </div>
+                        </div>
                     </div>
+                    <div className="row">
+                        <div className="file-field input-field col s6 offset-s5">
+                            <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                            <i className="material-icons right">send</i>
+                            </button>
+                        </div>
                     </div>
                 </form>
             </div>
