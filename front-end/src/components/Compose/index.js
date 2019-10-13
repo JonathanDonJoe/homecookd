cfd7066fe8fc473473d5f8e72018ad702e17a595
Anthony Pellingra <anthonypellingra@gmail.com>
2019-10-13 18:29:28 -0400

    send some messages to your events. refresh heavily.

--- front-end/src/components/Compose/index.js
+++ front-end/src/components/Compose/index.js
@@ -1,22 +1,57 @@
 import './Compose.css';
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import axios from 'axios'
+
 
 export class Compose extends Component {
-  postMessage = ()=> {
+
+  state = {
+    message: ''
+  }
+
+  postMessage = async (e)=> {
+    e.preventDefault()
+    let message = this.state.message;
+    let url = `${window.apiHost}/postMessage`
+    const requestData = {
+      message: message,
+      auth: this.props.auth,
+      eventId: this.props.conversation.conversation.id
+    }
+    const messageSent = await axios.post(url, requestData)
+    console.log(`message sent`);
+        this.setState({
+          message: ''
+        })
+
 
   }
+
+  changeMessage = (e) => {
+    this.setState({
+        message: e.target.value
+    })
+}
   render() {
     // console.log(this.props);
     return (
-      <div className="compose">
-        <input
-          type="text"
-          className="compose-input"
-          placeholder="Type a message, @name"
-        />
-      </div>
+      <form onSubmit={this.postMessage} className="col s12">
+          <div className="input-field col s12">
+              <textarea placeholder='Type your message' id="textarea1" value={this.state.message} onChange={this.changeMessage} maxLength={300} className="materialize-textarea"></textarea>
+              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+              <i className="material-icons right">send</i>
+              </button>
+          </div>
+      </form>
     );
   }
 }
+function mapStateToProps(state) {
+  return ({
+      auth: state.auth,
+      conversation: state.conversation
 
-export default Compose
+  })
+}
+export default connect(mapStateToProps)(Compose);
